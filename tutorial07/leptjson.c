ef043e2bb7e0d886a3350e3c48e411b31186eedf
wlbf <lbf1353@live.com>
2017-06-25 15:33:40 +0800

    07 done

--- tutorial07/leptjson.c
+++ tutorial07/leptjson.c
@@ -347,10 +347,36 @@ int lept_parse(lept_value* v, const char* json) {
 }
 
 static void lept_stringify_string(lept_context* c, const char* s, size_t len) {
-    /* ... */
+    size_t i, j;
+    char str[7];
+    PUTC(c, '\"');
+    for (i = 0, j = 0; i < len; ++i) {
+        if (s[i] < 0x20 || s[i] == '\"' || s[i] == '\\') {
+            if (i - j > 0)
+                PUTS(c, s + j, i - j);
+            j = i + 1;
+
+            switch (s[i]) {
+                case '\"': PUTS(c, "\\\"", 2); break;
+                case '\\': PUTS(c, "\\\\", 2); break;
+                case '\b': PUTS(c, "\\b", 2); break;
+                case '\f': PUTS(c, "\\f", 2); break;
+                case '\n': PUTS(c, "\\n", 2); break;
+                case '\r': PUTS(c, "\\r", 2); break;
+                case '\t': PUTS(c, "\\t", 2); break;
+                default:
+                    sprintf(str, "\\u%04X", s[i]);
+                    PUTS(c, str, 6);
+            }
+        }
+    }
+    if (i - j > 0)
+        PUTS(c, s + j, i - j);
+    PUTC(c, '\"');
 }
 
 static void lept_stringify_value(lept_context* c, const lept_value* v) {
+    int i;
     switch (v->type) {
         case LEPT_NULL:   PUTS(c, "null",  4); break;
         case LEPT_FALSE:  PUTS(c, "false", 5); break;
@@ -358,10 +384,25 @@ static void lept_stringify_value(lept_context* c, const lept_value* v) {
         case LEPT_NUMBER: c->top -= 32 - sprintf(lept_context_push(c, 32), "%.17g", v->u.n); break;
         case LEPT_STRING: lept_stringify_string(c, v->u.s.s, v->u.s.len); break;
         case LEPT_ARRAY:
-            /* ... */
+            PUTC(c, '[');
+            for (i = 0; i < v->u.a.size; ++i) {
+                lept_stringify_value(c, &v->u.a.e[i]);
+                if (i != v->u.a.size - 1)
+                    PUTC(c, ',');
+            }
+            PUTC(c, ']');
             break;
         case LEPT_OBJECT:
-            /* ... */
+            PUTC(c, '{');
+            for (i = 0; i < v->u.o.size; ++i) {
+                PUTC(c, '\"');
+                PUTS(c, v->u.o.m[i].k, v->u.o.m[i].klen);
+                PUTS(c, "\":", 2);
+                lept_stringify_value(c, &v->u.o.m[i].v);
+                if (i != v->u.o.size - 1)
+                    PUTC(c, ',');
+            }
+            PUTC(c, '}');
             break;
         default: assert(0 && "invalid type");
     }
